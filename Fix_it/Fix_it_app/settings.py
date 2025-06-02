d3e74a605b7b192b9f56536a825e4a5007f62402
mRanas123 <134073126+mRanas123@users.noreply.github.com>
2025-06-02 22:08:45 +0300

    Update settings.py

--- Fix_it/Fix_it_app/settings.py
+++ Fix_it/Fix_it_app/settings.py
@@ -119,6 +119,21 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+LOGGING_CONFIG = 'logging.config.dictConfig'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+}
 
 
 # Internationalization
